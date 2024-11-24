91fbc15981bf66519bd1fa8ac63f72b9a78e1e19
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-11-24 12:58:17 +0000

    updated package inits

--- src/decan/data/__init__.py
+++ src/decan/data/__init__.py
@@ -1,3 +1,9 @@
+"""
+DeCAN data package.
+
+Contains iterable datasets and utility modules.
+"""
+
 
 from .common_corpus import (
 	CommonCorpusDataset,
---
+++ src/decan/train/__init__.py
@@ -0,0 +1,6 @@
+"""
+DeCAN training package.
+
+Contains modules to train a DeCAN module using datasets from the `data` package,
+and utilities to checkpoint and configure the training pipeline.
+"""
