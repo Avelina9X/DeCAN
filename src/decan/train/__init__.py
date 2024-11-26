57a34bfdc203390908e54f534dfb80a46249f0c9
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-11-26 20:18:59 +0000

    added imports to train __init__

--- src/decan/train/__init__.py
+++ src/decan/train/__init__.py
@@ -4,3 +4,6 @@ DeCAN training package.
 Contains modules to train a DeCAN module using datasets from the `data` package,
 and utilities to checkpoint and configure the training pipeline.
 """
+
+from .trainer import Trainer
+from .configuration_trainer import TrainerConfig
