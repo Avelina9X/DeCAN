bacc05294d66f5d3e7ce3041531a6a4d7c6c354d
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2024-11-30 01:13:23 +0000

    re-enabled ddp optimization

--- src/decan/train/trainer.py
+++ src/decan/train/trainer.py
@@ -41,7 +41,7 @@ class Trainer:
         torch.backends.cuda.matmul.allow_tf32 = True # type: ignore # pylint: disable=W0212
         torch.backends.cudnn.allow_tf32 = True # type: ignore # pylint: disable=W0212
         torch._dynamo.config.cache_size_limit = 1024 * 1024 * 1024 # type: ignore # pylint: disable=W0212
-        torch._dynamo.config.optimize_ddp = False # type: ignore # pylint: disable=W0212
+        torch._dynamo.config.optimize_ddp = True # type: ignore # pylint: disable=W0212
         torch.backends.cuda.enable_cudnn_sdp( False )
     
         self.trainer_config = trainer_config
