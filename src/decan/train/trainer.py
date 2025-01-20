64a8b7a69904eb2574d2ed76ec37a69f968825d4
Avelina Kyriacou <lhk3@st-andrews.ac.uk>
2025-01-20 11:37:05 +0000

    added inductor hint for expanded block size

--- src/decan/train/trainer.py
+++ src/decan/train/trainer.py
@@ -62,7 +62,8 @@ class Trainer:
         torch.backends.cudnn.allow_tf32 = True # type: ignore # pylint: disable=W0212
         torch._dynamo.config.cache_size_limit = 1024 * 1024 * 1024 # type: ignore # pylint: disable=W0212
         torch._dynamo.config.optimize_ddp = True # type: ignore # pylint: disable=W0212
-        torch.backends.cuda.enable_cudnn_sdp( False )
+        torch.backends.cuda.enable_cudnn_sdp( False ) # type: ignore # pylint: disable=W0212
+        torch._inductor.runtime.hints.TRITON_MAX_BLOCK[ 'X' ] = 4096 # type: ignore # pylint: disable=W0212
         
         datasets.config.HF_DATASETS_TRUST_REMOTE_CODE = True # type: ignore
         datasets.disable_progress_bar()
