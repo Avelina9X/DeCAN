8ac651bd794eeac7703c313839d5fe6cb504d2d7
Avelina <lhk3@st-andrews.ac.uk>
2025-02-08 21:16:50 +0000

    changed import structure for main file

--- src/decan/__main__.py
+++ src/decan/__main__.py
@@ -2,18 +2,21 @@
 
 import sys
 
-import pretrain
-import evaluator
-
 if __name__ == '__main__':
     # Check the first argument to delegate to the correct module;
     # remaining args will be parsed in the corresponding setup()
     match sys.argv[1]: # type: ignore
 
         # Pretain module for, well, pretraining!
-        case 'pretrain': pretrain.setup()
+        case 'pretrain':
+            import pretrain
+            pretrain.setup()
 
-        case 'evaluate': evaluator.setup()
+        # Evaluation module for, well evaluation!
+        case 'evaluate':
+            import evaluator
+            evaluator.setup()
 
         # Invalid choice!
-        case _: raise ValueError( f'Invalid first argument `{sys.argv[1]}`' )
+        case _:
+            raise ValueError( f'Invalid first argument `{sys.argv[1]}`' )
