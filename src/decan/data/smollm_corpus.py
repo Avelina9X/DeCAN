0b20dd2a790f81686b673869d91ba0518301d8ec
Avelina <lhk3@st-andrews.ac.uk>
2025-02-15 00:09:06 +0000

    added filtering to remove files longer than 100k chars

--- src/decan/data/smollm_corpus.py
+++ src/decan/data/smollm_corpus.py
@@ -15,6 +15,8 @@ from transformers import PreTrainedTokenizerBase
 
 from .utils import base_batch_iterator, request_retry, read_lines_zst
 
+MAX_DOCUMENT_CHARS = 100_000
+
 class SmolLMCorpusClientDataset( IterableDataset ):
     """
     SmolLMCorpus Client IterableDataset which handles a single worker on a single device.
@@ -125,6 +127,8 @@ class SmolLMCorpusClientDataset( IterableDataset ):
                 try:
                     obj = json.loads(line)
                     text = obj[ 'text' ]
+                    if len( text ) > MAX_DOCUMENT_CHARS:
+                        continue
                     yield text
                 except ( KeyError, JSONDecodeError ):
                     print()
